bf5adfccd0bc54406bc54b64a082c58bf7ccaba1
tim de Paepe <tim@gmail.com>
2024-06-05 14:38:54 +0200

    zonder haakjes

--- src/services/PocketbaseService.jsx
+++ src/services/PocketbaseService.jsx
@@ -1,6 +1,6 @@
 import PocketBase from 'pocketbase';
 
-const pb = new PocketBase(`${process.env.REACT_APP_POCKETBASE_URL}`);
+const pb = new PocketBase(process.env.REACT_APP_POCKETBASE_URL);
 
 export default pb;
 
